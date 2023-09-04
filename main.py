d65551cacd13216a50222d55e9f4c26ca3714ab0
m.jari <m.jari@add.gov.ma>
2023-09-04 16:49:53 +0100

    code

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+# Load the trained model
+model = tf.keras.models.load_model("fraud_detection_model.h5")
+
+# Load the scaler used during training
+scaler = joblib.load("scaler.pkl")
+
+# Streamlit interface
+st.title("Fraud Detection")
+
+# Input fields for user to enter values
+distance_from_home = st.number_input("Distance from Home")
+distance_from_last_transaction = st.number_input("Distance from Last Transaction")
+ratio_to_median_purchase_price = st.number_input("Ratio to Median Purchase Price")
+repeat_retailer = st.checkbox("Repeat Retailer")
+used_chip = st.checkbox("Used Chip")
+used_pin_number = st.checkbox("Used PIN Number")
+online_order = st.checkbox("Online Order")
+
+# Predict button
+if st.button("Predict Fraud"):
+    input_data = np.array([
+        distance_from_home, distance_from_last_transaction, ratio_to_median_purchase_price,
+         float(repeat_retailer), float(used_chip), float(used_pin_number), float(online_order)
+    ])
+    
+    input_data = input_data.reshape(1, -1)
+    
+    # Standardize input data using the loaded scaler
+    input_data = scaler.transform(input_data)
+    
+    # Make a prediction
+    prediction = model.predict(input_data)
+    
+    # Display the result
+    if prediction >= 0.5:
+        st.error("Prediction: Fraudulent Transaction")
+    else:
+        st.success("Prediction: Not a Fraudulent Transaction")
