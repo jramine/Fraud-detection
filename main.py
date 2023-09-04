d51e3129987861e8959eb77c40bb2e0c879b1ef1
jramine <92678865+jramine@users.noreply.github.com>
2023-09-04 18:45:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import tensorflow as tf
 
-tf.keras.utils.get_custom_objects()['CustomAdam'] = CustomAdam
+CustomAdam = tf.keras.utils.get_custom_objects()['CustomAdam']
 
 # Load the Keras model
 model = tf.keras.models.load_model("fraud_detection_model.h5", custom_objects={'CustomAdam': CustomAdam})
